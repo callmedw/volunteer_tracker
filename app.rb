564a74ed152f5dc32db68c6978b6a07b62a790b0
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-05 16:56:18 -0700

    add sinatra function and erb frameworks

--- app.rb
+++ app.rb
@@ -6,6 +6,49 @@ also_reload('lib/**/*.rb')
 require 'pry'
 require 'pg'
 
-get('/') do
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get("/") do
+  @projects = Project.all()
   erb(:index)
+end
+
+post("/projects") do
+  project_title = params.fetch("project_title")
+  project = Project.new({:project_title => project_title, :id => nil})
+  project.save()
+  erb(:success)
+end
+
+get("/projects/:id") do
+  @project = Project.find(params.fetch("id").to_i())
+  erb(:project)
+end
+
+get("/projects/:id/edit") do
+  @project = Project.find(params.fetch("id").to_i())
+  erb(:project_edit)
+end
+
+patch("/projects/:id") do
+  project_title = params.fetch("project_title")
+  @project = Project.find(params.fetch("id").to_i())
+  @project.update({:project_title => project_title})
+  erb(:project)
+end
+
+delete("/projects/:id") do
+  @project = Project.find(params.fetch("id").to_i())
+  @project.delete()
+  @projects = Project.all()
+  erb(:index)
+end
+
+post("/volunteers") do
+  volunteer_name = params.fetch("volunteer_name")
+  project_id = params.fetch('project_id').to_i
+  @project = Project.find(project_id)
+  @volunteer = Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id})
+  @volunteer.save()
+  erb(:success)
 end
